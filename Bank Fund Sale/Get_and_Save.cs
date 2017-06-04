8bcb1da769587c9b988ba089bb6d7cfb3026e41c
Дмитрий Волынец < >
2017-06-04 19:08:45 +0300

    Добавьте файлы проекта.

---
+++ Bank Fund Sale/Get_and_Save.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Windows.Data.Json;
+using System.Net.Http;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Fund_Sale
+{
+    class Get_and_Save
+    {
+        public async string get_json_inf() // <<<<<<<<<<<<<<<<<<<<<<<<ВНИМАНИЕ!!!!! Поправить метод!! 
+        {
+            var client = new HttpClient(); // Add: using System.Net.Http;
+            var response = await client.GetAsync(new Uri("https://bankfund.sale/api/bidding?landing=true&limit=6&project=FG&state=in__completed,canceled,refused&way=auction"));
+            var result = await response.Content.ReadAsStringAsync();
+
+            JsonValue json = JsonValue.Parse(result);
+
+            string IDENTITY = json.GetObject().GetNamedString("identity");
+            string TITLE = json.GetObject().GetNamedString("title");
+            string URL = json.GetObject().GetNamedString("termsFileUrl");
+
+        }
+        async void json_inf_file() // <<<<<<<<<<<<<<<<<<<<<<< Написать условие создания файла, если его еще нет. Записать в мейн
+        {
+            // Create sample file; replace if exists.
+            Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            Windows.Storage.StorageFile sampleFile = await storageFolder.CreateFileAsync("sample.txt", Windows.Storage.CreationCollisionOption.ReplaceExisting);
+            sampleFile.OpenTransactedWriteAsync(get_json_inf.IDENTITY);
+        }
+    }
+}
---
+++ Bank Fund Sale/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace Bank_Fund_Sale
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            var json_obj = new App()
+            { };
+            App.get_json_inf();
+                {
+                json_obj.get_json_inf;
+                }
+            
+        }
+    }
+}
